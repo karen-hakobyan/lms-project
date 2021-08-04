81422eb7f4910d53e916ce3b40c8e83803e4a3b2
Karen Hakobyan <karenh.akobyan95real@gmail.com>
2021-08-04 18:06:25 +0400

    initial commit

---
+++ src/main/java/am/lms/LMSApp.java
@@ -0,0 +1,11 @@
+package am.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LMSApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LMSApp.class, args);
+    }
+}
